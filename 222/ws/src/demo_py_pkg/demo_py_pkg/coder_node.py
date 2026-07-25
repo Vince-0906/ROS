28cba246aeece04da09bea89c8ed637e419875da
Vince-0906 <Vince-0906@users.noreply.github.com>
2026-07-25 16:30:44 +0800

    2.5.1面向对象编程

--- 222/ws/src/demo_py_pkg/demo_py_pkg/coder_node.py
+++ 222/ws/src/demo_py_pkg/demo_py_pkg/coder_node.py
@@ -1,14 +1,19 @@
 from demo_py_pkg.person_node import PersonNode
+import rclpy
+from rclpy.node import Node
 
 class CoderNode(PersonNode):
-    def __init__(self, name_value: str, age_value: int, code_value: str) -> None:
+    def __init__(self, node_name: str, name_value: str, age_value: int, code_value: str) -> None:
         print('CoderNode__init__被调用了')
-        super().__init__(name_value,age_value) #调用父类的构造函数
+        super().__init__(node_name, name_value, age_value) #调用父类的构造函数
         self.code = code_value
-    def coder(sele):
+    def coder(self):
         pass
 
 def main():
-    node = CoderNode('Cindy','66','Python')
+    rclpy.init()
+    node = CoderNode('node_Cindy','Cindy','66','Python')
     node.eat('面包')
     print(f'爱写{node.code}代码')
+    rclpy.spin(node)
+    rclpy.shutdown()
