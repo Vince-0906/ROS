141ebe35295b6a9e0b3f9ee46b2b3678e99a4985
Vince-0906 <Vince-0906@users.noreply.github.com>
2026-07-28 16:48:42 +0800

    3.3.2订阅pose实现闭环控制cpp

---
+++ 333/topic_ws/src/demo_cpp_topic/src/turtle_control.cpp
@@ -0,0 +1,77 @@
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <chrono>
+#include <turtlesim/msg/pose.hpp>
+
+using namespace std::chrono_literals;
+
+class TurtleControl: public rclcpp::Node
+{
+    private:
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;//发布者的共享指针
+        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscriber_;//订阅者的共享指针
+        double target_x_ = {1.0}; // 目标位置的 x 坐标
+        double target_y_ = {1.0}; // 目标位置的 y 坐标
+        double k_={3.0}; // 比例增益
+        double max_speed_={3.0}; // 最大速度
+
+
+    public:
+        explicit TurtleControl(const std::string &node_name): Node(node_name)
+        {
+            publisher_= this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+            subscriber_ =this ->create_subscription<turtlesim::msg::Pose>("turtle1/pose", 10,std::bind(&TurtleControl::on_pose_received, this,std::placeholders::_1));
+        }
+
+
+
+        void on_pose_received(const turtlesim::msg::Pose::SharedPtr pose) //参数:收到数据的共享指针
+        {
+            //Todo:根据小乌龟的位姿信息控制小乌龟运动
+            // 1.获取当前位置
+            auto current_x = pose->x;
+            auto current_y = pose->y;
+            RCLCPP_INFO(get_logger(), "当前位置: x=%f, y=%f", current_x, current_y);
+
+            //2.计算海龟当前位置与目标位置的距离差和角度差
+            auto distance =std::sqrt(
+                (target_x_ - current_x) * (target_x_ - current_x)+
+                (target_y_ - current_y) * (target_y_ - current_y)
+            );
+
+            auto angle =std::atan2((target_y_ - current_y), (target_x_ - current_x))-pose->theta;
+
+
+            RCLCPP_INFO(get_logger(), "距离差: %f,角度差: %f", distance,angle);
+
+            // 3.根据距离和角度差计算速度
+            auto msg = geometry_msgs::msg::Twist();
+            if(distance > 0.1)
+            {
+                if(fabs(angle)>0.2){
+                    msg.angular.z = fabs(angle);
+                }else{
+                    msg.linear.x = k_ * distance;
+                }
+            }
+
+            //4.限制速度最大值
+            if(msg.linear.x > max_speed_)
+            {
+                msg.linear.x = max_speed_;
+            }
+
+            publisher_->publish(msg);
+        }
+};
+
+
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node= std::make_shared<TurtleControl>("turtle_control");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
