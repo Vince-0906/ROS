24100d3e84a3161c7027b97926170828db3ec772
Vince-0906 <Vince-0906@users.noreply.github.com>
2026-07-29 17:25:04 +0800

    4.2.2人脸检测 py

---
+++ 444/ws/src/demo_py_service/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'demo_py_service'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+"/resource", ['resource/default.jpeg']),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='szc',
+    maintainer_email='Vince-0906@users.noreply.github.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'learn_face_detect = demo_py_service.learn_face_detect:main',
+        ],
+    },
+)
