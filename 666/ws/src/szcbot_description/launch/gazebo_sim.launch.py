822798d0dc7376dfd601cc7f253c15ce63fac072
Vince-0906 <Vince-0906@users.noreply.github.com>
2026-08-03 23:19:56 +0800

    6.4.4 使用两轮差速插件控制机器人

--- 666/ws/src/szcbot_description/launch/gazebo_sim.launch.py
+++ 666/ws/src/szcbot_description/launch/gazebo_sim.launch.py
@@ -2,23 +2,26 @@ import launch
 import launch_ros
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ros_gz_bridge.actions import RosGzBridge
 
 def generate_launch_description():
     # 获取功能包share路径
     urdf_package_path = get_package_share_directory('szcbot_description')
     default_xacro_path = os.path.join(urdf_package_path, 'urdf', 'szcbot/szcbot.urdf.xacro')
-    # default_rviz_config_path = os.path.join(urdf_package_path, 'config', 'display_robot_model.rviz')
     default_gazebo_world_path = os.path.join(urdf_package_path, 'world', 'room.sdf')
+    default_bridge_yaml_path = os.path.join(urdf_package_path, 'config', 'bridge.yaml')
+
     # 声明一个xacro目录参数，方便修改
-    action_declare_arg_model_path = launch.actions.DeclareLaunchArgument(
+    action_declare_arg_model_path = DeclareLaunchArgument(
         name='model',
-        default_value= str(default_xacro_path),
+        default_value=str(default_xacro_path),
         description='加载模型的xacro文件路径'
     )
 
     # 通过文件路径获取内容，并转换为参数值对象，以供传入robot_state_publisher
-
-    command_result = launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
+    command_result = launch.substitutions.Command(['xacro ', LaunchConfiguration('model')])
     robot_description_value = launch_ros.parameter_descriptions.ParameterValue(command_result, value_type=str)
 
     action_robot_state_publisher = launch_ros.actions.Node(
@@ -29,27 +32,49 @@ def generate_launch_description():
 
     action_launch_gazebo = launch.actions.IncludeLaunchDescription(
         launch.launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch','gz_sim.launch.py')
+            [get_package_share_directory('ros_gz_sim'), '/launch', '/gz_sim.launch.py']
         ),
-        launch_arguments=[('gz_args', default_gazebo_world_path),('verbose', 'true')],  # 传入gazebo世界文件路径参数
+        launch_arguments={'gz_args': f'-r {default_gazebo_world_path}'}.items()
     )
 
     action_create = launch_ros.actions.Node(
         package='ros_gz_sim',
         executable='create',
-        arguments=["-topic", "/robot_description", "-name", "szcbot",
-                "-x", "0.0",
-                "-y", "0.0",
-                "-z", "0.0",
-                '-world', 'room'],
-        output="screen",
+        arguments=['-topic', '/robot_description', '-name', 'szcbot',
+                   '-x', '0.0',
+                   '-y', '0.0',
+                   '-z', '0.01',
+                   '-world', 'room'],
+        output='screen',
         parameters=[{'use_sim_time': True}]
-)
+    )
 
+    # 桥接配置参数声明
+    bridge_name_arg = DeclareLaunchArgument(
+        'bridge_name',
+        default_value='szcbot_bridge',
+        description='Name of ros_gz_bridge node'
+    )
+
+    config_file_arg = DeclareLaunchArgument(
+        'config_file',
+        default_value=default_bridge_yaml_path,
+        description='YAML config file'
+    )
+
+    # 使用 RosGzBridge action 来桥接话题
+    bridge = RosGzBridge(
+        bridge_name=LaunchConfiguration('bridge_name'),
+        config_file=LaunchConfiguration('config_file')
+    )
 
     return launch.LaunchDescription([
         action_declare_arg_model_path,
+        bridge_name_arg,
+        config_file_arg,
         action_robot_state_publisher,
         action_launch_gazebo,
-        action_create
+        action_create,
+        bridge
     ])
+
