4e6e06c174c3b4c448833c6eddc60199b9aca6d5
Vince-0906 <Vince-0906@users.noreply.github.com>
2026-08-03 15:48:53 +0800

    6.2.3 使用Xacro简化URDF

--- 666/ws/src/szcbot_description/launch/display_robot.launch.py
+++ 666/ws/src/szcbot_description/launch/display_robot.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     # 通过文件路径获取内容，并转换为参数值对象，以供传入robot_state_publisher
 
-    command_result = launch.substitutions.Command(['cat ', launch.substitutions.LaunchConfiguration('model')])
+    command_result = launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
     robot_description_value = launch_ros.parameter_descriptions.ParameterValue(command_result, value_type=str)
 
     action_robot_state_publisher = launch_ros.actions.Node(
